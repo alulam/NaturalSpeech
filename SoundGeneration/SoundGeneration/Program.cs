5bbe6d45ae2191ff92865cfca6fb3717029559fa
roadeo7 <roadeo7@gmail.com>
2016-03-02 21:46:34 -0500

    implement speak

---
+++ SoundGeneration/SoundGeneration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace SoundGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string word in args)
+            {
+                using (FileStream f = new FileStream(word + ".wav", FileMode.Create))
+                {
+                    using (SpeechSynthesizer speak = new SpeechSynthesizer())
+                    {
+                        speak.SetOutputToWaveStream(f);
+                        speak.Speak(word);
+                    }
+                }
+            }
+        }
+    }
+}
